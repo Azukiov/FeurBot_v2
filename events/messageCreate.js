9dcd6c6558cbc95e76a8f01da88c4634afeba056
Its_Azukio <augustin.pro.prs@gmail.com>
2023-02-12 16:45:34 +0100

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,9 +1,24 @@
+const quoi = [
+    "quoi",
+    "qwa",
+    "koi",
+    "kwa",
+    "pourquoi",
+    "pourkoi",
+    "pourkwa",
+    "pourqwa"
+]
+
 module.exports = {
     name: 'messageCreate',
 
     async execute(message) {
         if (message.author.bot) return;
-        if (message.channel.type == 'dm')
-            return console.log("DMs are not supported yet!")
+        if (message.channel.type === 'DM') return;
+
+
+        if (quoi.includes(message.content)) {
+            message.reply("Feur")
+        }
     }
 }
