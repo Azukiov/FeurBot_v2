9b6eb7d59a240cf26f8bc9e80db74d526cf450d1
Its_Azukio <augustin.pro.prs@gmail.com>
2023-02-12 15:02:35 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,6 +6,6 @@ module.exports = {
     execute(client) {
         console.log(`Logged in as ${client.user.tag}!`);
 
-        client.user.setActivity('la V2', { type: ActivityType.Watching });1e4
+        client.user.setActivity('la v2', { type: ActivityType.Watching });1e4
     }
 }
